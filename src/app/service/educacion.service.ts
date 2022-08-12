cae2a6d2c89871b9e7a95f31a519f6847851c0e3
Luis G. Martinez <99708664+luis-guillermo-martinez@users.noreply.github.com>
2022-08-12 00:14:37 -0300

    Cambios

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL= 'http://localhost:8080/educ/';
+  URL= 'https://backendlgm.herokuapp.com/educ/';
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Educacion[]>{
